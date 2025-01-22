21b6abd2a26ee96c171798eda1179d24a85c37df
HansGit30 <homgit07@gmail.com>
2025-01-22 15:21:15 -0500

    cambio

--- src/pages/Cover.jsx
+++ src/pages/Cover.jsx
@@ -7,11 +7,36 @@ import TortaAlfajor from "../assets/tortaAlfajor.jpeg"
 const Cover = () => {
   return (
     <section className="cover relative w-full h-screen overflow-hidden font-montserrat">
-      <div className="superior absolute -top-[20%] -right-[50%] bg-slate-400 w-[400px] h-[400px] rounded-full"></div>
 
-      <header className="relative w-full h-[50%] pl-4 pt-5">
+      <header className="w-full mb-5 flex justify-center">
+        <img className="w-[120px] mb-5 pl-4" src={Logo} alt="" />
+      </header>
+
+      <aside className="relative w-full h-[50%] flex justify-center items-start">
+        <div className="absolute left-0 bottom-0 -rotate-12 w-[180px] h-[250px] rounded-2xl z-[1]">
+          <img className="w-full h-full object-cover rounded-2xl" src={Galletas} alt="" />
+        </div>
+        <div className="w-[180px] h-[250px]  rounded-t-2xl z-[2]">
+          <img className="w-full h-full object-cover rounded-2xl" src={MiniTorta} alt="" />
+        </div>
+        <div className="absolute -right-5 bottom-7 rotate-12 w-[180px] h-[250px] rounded-2xl z-[3]">
+          <img className="w-full h-full object-cover rounded-2xl" src={TortaAlfajor} alt="" />
+        </div>
+{/* 
+        <div className="btnStart absolute z-[4] top-[55%] w-[250px] h-[70px] rounded-2xl text-white flex items-center justify-center text-xl">Elige tu favorito</div> */}
+      </aside>
+
+      <div className="w-full text-center mt-10">
+        <h1 className="text-xl mb-5">Postres perfectos para celebrar el amor y la amistad</h1>
+        <button className="btnStart px-10 py-5 rounded-2xl text-white">Elige tu favorito</button>
+      </div>
+
+
+
+
+      {/* <header className="relative w-full h-[50%] pl-4 pt-5">
         <img className="w-[120px] mb-8" src={Logo} alt="" />
-        <h1 className="max-w-[320px] text-5xl font-outfit leading-tight text-white">Dulzura para compartir este San Valentín</h1>
+        <h1 className="max-w-[320px] text-5xl font-outfit leading-tight">Dulzura para compartir este San Valentín</h1>
       </header>
 
       <aside className="relative w-full h-[50%] flex justify-center items-end">
@@ -26,7 +51,7 @@ const Cover = () => {
         </div>
 
         <div className="btnStart absolute z-[4] top-[55%] w-[250px] h-[70px] rounded-2xl text-white flex items-center justify-center text-xl">Elige tu favorito</div>
-      </aside>
+      </aside> */}
     </section>
   )
 }
