be2fa5a8fba199409faeabd6cf58c2819c515e59
HansGit30 <homgit07@gmail.com>
2025-01-23 16:08:30 -0500

    home2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -29,30 +29,30 @@ const Home = () => {
             </div>
         </section>
         <section className="w-full px-4">
-                <h2 className="text-2xl mb-2">Nos tomamos en serio nuestros postres artesanales</h2>
+                <h2 className="text-2xl mb-2 font-bold">Nos tomamos en serio nuestros postres artesanales</h2>
                 <p className="mb-10">Ingredientes frescos, recetas únicas y un toque especial.</p>
 
                 <article className="w-full">
                     <div className="w-[200px] h-[250px] bg-slate-500 rounded-lg mb-4"></div>
                     <div className="mb-10">
-                        <h3>Detalles Únicos y Cuidado en la Presentación</h3>
-                        <p>Los postres artesanales suelen estar decorados con un toque especial y estético, ideal para regalar o para ocasiones especiales.</p>
+                        <h3 className="font-bold text-xl max-w-[250px] mb-2">Detalles Únicos y Cuidado en la Presentación</h3>
+                        <p className="max-w-[260px]">Nuestros postres suelen estar decorados con un toque especial y estético, ideal para regalar o para ocasiones especiales.</p>
                     </div>
                 </article>
 
                 <article className="w-full flex flex-col items-end">
                     <div className="w-[200px] h-[250px] bg-slate-500 rounded-lg mb-4"></div>
                     <div className="mb-10">
-                        <h3 className="text-end">Calidad de los Ingredientes</h3>
-                        <p className="text-end">Los postres artesanales suelen prepararse con ingredientes frescos y de alta calidad, sin conservantes ni aditivos artificiales. Esto asegura un mejor sabor y mayor valor nutritivo.</p>
+                        <h3 className="text-end font-bold text-xl mb-2">Calidad de los Ingredientes</h3>
+                        <p className="text-end max-w-[260px]">Preparamos con ingredientes frescos y de alta calidad, sin conservantes ni aditivos artificiales. Esto asegura un mejor sabor y mayor valor nutritivo.</p>
                     </div>
                 </article>
 
                 <article className="w-full">
                     <div className="w-[200px] h-[250px] bg-slate-500 rounded-lg mb-4"></div>
                     <div className="mb-5">
-                        <h3>Sabor Único y Auténtico</h3>
-                        <p>Cada receta es cuidadosamente elaborada, lo que da como resultado postres con un sabor más auténtico y personalizado, alejándose de la producción masiva.</p>
+                        <h3 className="font-bold text-xl mb-2">Sabor Único y Auténtico</h3>
+                        <p className="max-w-[260px]">Cada receta es cuidadosamente elaborada, lo que da como resultado postres con un sabor más auténtico y personalizado.</p>
                     </div>
                 </article>
 
