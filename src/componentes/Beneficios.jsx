48a770360629f5ee8fdc2bd8b7c5d5877d8060e8
HansGit30 <homgit07@gmail.com>
2025-01-29 18:42:25 -0500

    imagenes

--- src/componentes/Beneficios.jsx
+++ src/componentes/Beneficios.jsx
@@ -1,4 +1,6 @@
-
+import receta from "../assets/receta.jpg"
+import detalles from "../assets/detalles.webp"
+import ingre from "../assets/ingredientes.jpg"
 
 const Beneficios = () => {
     return (
@@ -7,7 +9,9 @@ const Beneficios = () => {
             <p className="mb-10 lg:text-sm lg:px-10">Ingredientes frescos, recetas únicas y un toque especial.</p>
             <div className="lg:flex items-center justify-between lg:px-10">
                 <article className="w-full">
-                    <div className="w-[200px] h-[250px] bg-slate-500 rounded-lg mb-4"></div>
+                    <div className="w-[200px] h-[250px] bg-slate-500 rounded-lg mb-4">
+                        <img className="w-full h-full object-cover rounded-lg" src={detalles} alt="detalles" />
+                    </div>
                     <div className="mb-10">
                         <h3 className="font-bold text-xl max-w-[250px] mb-2 font-montserrat lg:text-lg">Detalles Únicos en la Presentación</h3>
                         <p className="max-w-[260px]">Nuestros postres suelen estar decorados con un toque especial y estético, ideal para regalar o para ocasiones especiales.</p>
@@ -15,7 +19,9 @@ const Beneficios = () => {
                 </article>
 
                 <article className="w-full flex flex-col items-end md:items-center">
-                    <div className="w-[200px] h-[250px] bg-slate-500 rounded-lg mb-4"></div>
+                    <div className="w-[200px] h-[250px] bg-slate-500 rounded-lg mb-4">
+                        <img className="w-full h-full object-cover rounded-lg" src={ingre} alt="detalles" />
+                    </div>
                     <div className="mb-10">
                         <h3 className="font-montserrat text-end font-bold text-xl mb-2 lg:text-lg">Calidad de los Ingredientes</h3>
                         <p className="text-end max-w-[270px]">Preparamos con ingredientes frescos y de alta calidad, sin conservantes ni aditivos artificiales. Esto asegura un mejor sabor y mayor valor nutritivo.</p>
@@ -23,7 +29,9 @@ const Beneficios = () => {
                 </article>
 
                 <article className="w-full flex flex-col md:items-end">
-                    <div className="w-[200px] h-[250px] bg-slate-500 rounded-lg mb-4"></div>
+                    <div className="w-[200px] h-[250px] rounded-lg mb-4">
+                        <img className="w-full h-full object-cover rounded-lg" src={receta} alt="receta" />
+                    </div>
                     <div className="mb-5 md:text-end">
                         <h3 className="font-bold text-xl mb-2 font-montserrat lg:text-lg">Sabor Único y Auténtico</h3>
                         <p className="max-w-[260px]">Cada receta es cuidadosamente elaborada, lo que da como resultado postres con un sabor más auténtico y personalizado.</p>
